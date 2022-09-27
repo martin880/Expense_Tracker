3895b385c8998f2c5e9ca2f37779cdb2d7d25823
Martin Lumban Gaol <martinlumbangaol880@gmail.com>
2022-09-27 21:41:53 +0700

    Add project files.

---
+++ Expense_Tracker/Models/Category.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+        [Column(TypeName ="nvarchar(50)")]
+        public string Title { get; set; }
+        [Column(TypeName = "nvarchar(5)")]
+        public string Icon { get; set; } = "";
+        [Column(TypeName = "nvarchar(10)")]
+        public string Type { get; set; } = "Expense";
+        [NotMapped] // Tidak berdampak pada database
+        public string? TitleWithIcon {
+            get 
+            {
+                return this.Icon + " " + this.Title;
+            } 
+        }
+
+    }
+}
---
+++ Expense_Tracker/Program.cs
@@ -0,0 +1,33 @@
+using Expense_Tracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// inject the database
+builder.Services.AddDbContext<ApplicationDBContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+
+//Register Syncfusion license => untuk tampilan frontend
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Mgo+DSMBMAY9C3t2VVhiQlFaclxJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRdkxhXn9ecXVVR2dUVkw=");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
